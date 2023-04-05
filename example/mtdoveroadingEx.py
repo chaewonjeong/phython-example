757e6a6e2e994e9e3ef36b6fcda192ba62e08d80
chaewonjeong <chaewonjeong.dev@gmail.com>
2023-04-05 14:24:14 +0900

    20230405 Study

--- example/mtdoveroadingEx.py
+++ example/mtdoveroadingEx.py
@@ -1,20 +1,28 @@
+# 메서드 오버로딩 : 부모 클래스에서 정의한 메서드 말고 자식클래스에서 정의한 메서드를 쓰고싶을때 메서드를 새롭게 정의해서 쓰고 싶을때
+
 # 일반 유닛
 # 부모 클래스
 class Unit:
     # __init__ : 생성자
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         # 멤버변수 : 클래스 내에서 정의된 변수
         self.name = name
         self.hp = hp
+        self.speed = speed
         print("{0}유닛이 생성되었습니다.".format(self.name))
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"
+               .format(self.name, location, self.speed))
 
 
 # 공격 유닛 : 일반 유닛을 상속받아서 만들어짐
 # 자식 클래스
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, speed, damage,):
         # 일반 유닛에서 만들어진 생성자를 호출해서 name과 hp를 정의
-        Unit.__init__(self, name, hp)
+        Unit.__init__(self, name, hp, speed)
         # 추가로 공격력을 정의
         self.damage = damage
 
@@ -30,13 +38,9 @@ class AttackUnit(Unit):
         if self.hp <= 0:
             print("{0} : 파괴되었습니다.".format(self.name))
 
-
-# 다중 상속
-# 부모클래스를 2개이상 상속받는것
-# 공중유닛
-# 드랍쉽 : 공중 유닛, 수송기, 공격불가능
-
 # 날 수 있는 기능을 가진 클래스
+
+
 class Flyable():
     def __init__(self, flying_speed):
         self.flying_speed = flying_speed
@@ -49,18 +53,13 @@ class Flyable():
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage)  # 지상 speed 0
         Flyable.__init__(self, flying_speed)
 
 
-# 파이어뱃 : 공격 유닛, 화염방사기.
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("벌쳐", 80, 10, 20)
 
-firebat1.damaged(25)
-firebat1.damaged(25)
-
-
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "5시")
+# 배틀크루져 : 공중 유닛
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+battlecruiser.fly(battlecruiser.name, "5시")
